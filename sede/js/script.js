e14ff4e289e171cfcf8f17099c28704d7f35c8e6
carlatin <robinsonemanuelopez@gmail.com>
2019-06-12 19:51:23 -0600

    full pokedex

--- sede/js/script.js
+++ sede/js/script.js
@@ -1,7 +1,7 @@
 var pokemon= angular.module("pokemon",[])
 	pokemon.controller("listadopokemon",function($scope,$rootScope,$http){
 		$scope.Listado=[];
-		for(var i = 1; i<100;i++){
+		for(var i = 0; i<100;i++){
 				$http({
 				method:"GET",
 				url:"https://pokeapi.co/api/v2/pokemon/"+i
