36a89d156bd7c7516bf2228f7791948936f446db
carlatin <robinsonemanuelopez@gmail.com>
2019-06-15 11:05:12 -0600

    apex

--- sede/js/script.js
+++ sede/js/script.js
@@ -1,19 +1,18 @@
 var pokemon= angular.module("pokemon",[])
 	pokemon.controller("listadopokemon",function($scope,$rootScope,$http){
 		$scope.Listado=[];
-		for(var i = 0; i<100;i++){
+		for(var i =0; i<100;i++){
 				$http({
 				method:"GET",
 				url:"https://pokeapi.co/api/v2/pokemon/"+i
 			}).then(function success(y){
 				$scope.Listado.push(y);
 				console.log("p.data.sprites.front_default")
-				for(var imagen=1;imagen<100;imagen++){
+				for(var imagen=0;imagen<100;imagen++){
 					if(imagen<10){
-						$scope.Listado[imagen].data.sprites.front_default="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00"+imagen+".png"
+						$scope.Listado[imagen].data.sprites.front_default="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00"+$scope.Listado[imagen].data.id+".png"
 					}else if (imagen>9){
-						$scope.Listado[imagen].data.sprites.front_default="https://assets.pokemon.com/assets/cms2/img/pokedex/full/0"+imagen+".png"
-					}
+						$scope.Listado[imagen].data.sprites.front_default="https://assets.pokemon.com/assets/cms2/img/pokedex/full/0"+$scope.Listado[imagen].data.id+".png"					}
 				}
 			})
 		}
